3d12d04bb2f838af1200494a46ccd95ad9a0dc5c
kaliv0 <kaloyan.ivanov88@gmail.com>
2024-05-09 20:11:05 +0300

    added argsparse and consts

--- handle.py
+++ handle.py
@@ -1,7 +1,15 @@
+import argparse
 import logging
 import os
 import re
 from dataclasses import dataclass
+
+
+IMPORT_PATTERN = " {imp}[.(]"
+IMPORT_KEYWORD_LEN = len("import") + 1
+BAD_PRACTICE_ERROR = "Bad practice using import *"
+CLEANUP_FAILED_ERROR = "Something went wrong while cleaning up unused imports"
+TEMP_TEMPLATE = "{file}.swap"
 
 
 @dataclass
@@ -12,11 +20,12 @@ class ImportData:
 
 
 class Cleaner:
-    def __init__(self, file, temp_file, skip_commented):
+    def __init__(self, file, skip_commented):
+        self.file = file
+        self.temp_file = TEMP_TEMPLATE.format(file=file)
         # load file in memory
         with open(file, "r") as f:
             self.lines = f.readlines()
-        self.temp_file = temp_file
         self.skip_commented = skip_commented
         self.imp_map = {}
         self.line_num = -1
@@ -27,10 +36,9 @@ class Cleaner:
             # skip commented out imports
             if line.startswith(("#", "\n")):
                 continue
-            # handle aliases
+
             if len(imp := line.split("as")) > 1:
                 self.handle_aliases(imp[1].rstrip("\n"))
-            # handle regular imports (multiple, separated by comma on same line)
             elif len(imp := line.split("import")) > 1:
                 self.handle_imports(imp[1].rstrip("\n"))
             else:
@@ -40,8 +48,9 @@ class Cleaner:
         self.imp_map[imp.strip()] = ImportData(self.line_num, 0, False)
 
     def handle_imports(self, imp):
-        if "*" in imp:
-            raise ValueError("Bad practice using import *")
+        if imp.startswith("*"):
+            raise ValueError(BAD_PRACTICE_ERROR)
+
         imp_list = imp.split(",")
         for imp in imp_list:
             is_mult = len(imp_list) > 1
@@ -53,10 +62,9 @@ class Cleaner:
                 continue
             for imp in self.imp_map:
                 # TODO: extract regex as const
-                if re.search(f" {imp}[.(]", line):
+                if re.search(IMPORT_PATTERN.format(imp=imp), line):
                     # track import usage
                     self.imp_map[imp].count += 1
-        print(self.imp_map)
 
     def write_imports(self, f):
         for i, line in enumerate(self.lines[: self.line_num], 0):
@@ -72,8 +80,9 @@ class Cleaner:
                 line = self.prepare_import_line(imp_list, line)
             f.write(line)
 
-    def prepare_import_line(self, imp_list, line):
-        return line[: line.find("import") + len("import") + 1] + ", ".join(imp_list) + "\n"
+    @staticmethod
+    def prepare_import_line(imp_list, line):
+        return line[: line.find("import") + IMPORT_KEYWORD_LEN] + ", ".join(imp_list) + "\n"
 
     def should_write_import_line(self, line):
         # TODO: pass as flag by user
@@ -85,11 +94,10 @@ class Cleaner:
     def build_multiple_import_list(self, i):
         imp_list = []
         for imp, val in list(self.imp_map.items()):
-            # TODO: fix if statements
             if val.line_num == i:
                 if val.count == 0 and val.is_multiple is False:
                     return True, imp_list
-                elif val.count > 0:
+                if val.count > 0:
                     imp_list.append(imp)
         return False, imp_list
 
@@ -107,15 +115,17 @@ class Cleaner:
             self.read_imports()
             self.read_rest_of_file()
             self.write_to_temp_file()
-        except (RuntimeError, Exception) as e:
-            logging.error(e)
-            if os.path.exists(temp_file):
-                os.remove(temp_file)
+        except (RuntimeError, Exception):
+            logging.error(CLEANUP_FAILED_ERROR)
+            if os.path.exists(self.temp_file):
+                os.remove(self.temp_file)
         else:
-            os.replace(temp_file, file)
+            os.replace(self.temp_file, self.file)
 
 
 if __name__ == "__main__":
-    file = "foo.py"
-    temp_file = file + ".swap"
-    Cleaner(file, temp_file, True).clean_imports()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path")
+    args = parser.parse_args()
+    # file_path = "foo.py"
+    Cleaner(args.file_path, True).clean_imports()
